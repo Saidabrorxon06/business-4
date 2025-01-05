d94d1bce324cd4f46677e50a0acdc387ca08969b
Saidabrorxon06 <saidabrorahmadov@icloud.com>
2025-01-05 05:51:38 +0500

    Add files via upload

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://NkOgAoa793iJdwNuhQQmJnVEhCpAyC9z_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "AH3-Ethics and social responsibility"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;&lt;span style=&#34;font-size: 14px;&#34;&gt;To achieve high performance in almost any type of commercial activity, each participant in business communication must have a certain set of information, knowledge, ideas about the rules, forms, and methods of doing business.&lt;br&gt;&lt;br&gt;Knowledge of the principles of business ethics predetermines the establishment and development of relations between participants in business communication, largely determining their effectiveness. Whether these relationships will be successfully implemented in the interests of partners or will become meaningless, ineffective, or completely stop depending on an understanding of the laws and principles of business ethics.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&#34;font-size: 14px;&#34;&gt;&lt;br&gt;&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&#34;font-size: 14px;&#34;&gt;The international business community and the international business community value the positive opinion of partners, which opens up broad prospects for communication participants for establishing new economic ties.&lt;br&gt;&lt;br&gt;Business ethics is based on the rules and norms of behavior developed by more than one generation of people around the world, which help to strengthen mutual trust and exclude the &amp;quot;use&amp;quot; of a business partner.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&#34;font-size: 14px;&#34;&gt;&amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp; &amp;nbsp;&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&#34;font-size: 14px;&#34;&gt;Mastering the science of business communications, including the norms of behavior, etiquette, as well as business contacts, it is necessary to take into account that business is the art of establishing and maintaining civilized relationships with people equal to itself, leading, managing with the help of power and responsibility, overcoming contradictions, resolving conflicts, take the role of a mediator, exchange information, be able to process it and effectively convert it in favor of a certain social group. And therefore, without developing the rules of the game, taking into account the interests of various categories of participants in business communication, it is impossible to effectively move towards the goal.&lt;/span&gt;&lt;/p&gt;"
+};
